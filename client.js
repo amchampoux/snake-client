0385d4552c4cc28eb7ca06ffa33d66e872f2f3bb
amchampoux <amchampoux@mirego.com>
2022-11-15 23:07:48 -0500

    add name and log on connection

--- client.js
+++ client.js
@@ -1,4 +1,5 @@
 const net = require("net");
+const { connected } = require("process");
 
 // establishes a connection with the game server
 const connect = function() {
@@ -6,6 +7,13 @@ const connect = function() {
     host: '10.0.0.41',
     port: 50541,
   });
+
+  // print a message to the screen when the connection is successfully established.
+  conn.on("connect", () => {
+    console.log("Successfully connected to game server");
+    conn.write("Name: AMC");
+  });
+
 
   // interpret incoming data as text
   conn.setEncoding("utf8");

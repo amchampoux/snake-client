fa8aafe52377cc68cd1b16c20e71430f3975712a
amchampoux <amchampoux@mirego.com>
2022-11-16 17:16:05 -0500

    add user input listener

--- client.js
+++ client.js
@@ -4,16 +4,33 @@ const { connected } = require("process");
 // establishes a connection with the game server
 const connect = function() {
   const conn = net.createConnection({
-    host: '10.0.0.41',
+    host: '10.3.1.225',
     port: 50541,
   });
 
   // print a message to the screen when the connection is successfully established.
   conn.on("connect", () => {
     console.log("Successfully connected to game server");
+    // Send name to server as soon as connected
     conn.write("Name: AMC");
   });
 
+  // conn.on("connect", () => {
+    // Send move instruction to server as soon as connected
+    // setTimeout(() => {
+    //   conn.write("Move: up");
+    // }, 50);
+    // setTimeout(() => {
+    //   conn.write("Move: left");
+    // }, 100);
+    // setTimeout(() => {
+    //   conn.write("Move: down");
+    // }, 150);
+    // setInterval(() => {
+    //   conn.write("Move: down");
+    // }, 50);
+    
+  // });
 
   // interpret incoming data as text
   conn.setEncoding("utf8");

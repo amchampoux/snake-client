52d1a8c7eb53e5298bbd53f98338d341343c0fcd
amchampoux <amchampoux@mirego.com>
2022-11-16 22:48:22 -0500

    move constant values in module

--- client.js
+++ client.js
@@ -1,11 +1,12 @@
 const net = require("net");
 const { connected } = require("process");
+const { IP, PORT } = require('./constants');
 
 // establishes a connection with the game server
 const connect = function() {
   const conn = net.createConnection({
-    host: '10.0.0.41',
-    port: 50541,
+    host: IP,
+    port: PORT,
   });
 
   // print a message to the screen when the connection is successfully established.
